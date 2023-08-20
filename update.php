e74f36c6344c5a36f23ded57ce2c27d9788b5b3f
Shrushtiiii <pimpalkarshrushti@gmail.com>
2023-08-20 22:28:41 +0530

    Second Commit

--- update.php
+++ update.php
@@ -1,6 +1,3 @@
-<?php
-session_start();
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -79,10 +76,42 @@ session_start();
       <div class="container">
         <div class="row align-items-center">
           <div class="col-lg-5 ml-auto">
-            <h1 class="text-white">SPORTS' EVENT WEBSITE</h1>
-            <h3>You miss 100% of shots you don't take.</h3>
-             
-                         
+            <form action='logincheck.php' method='post'>
+</br></br> <table align='left' border=0 style="background-color:transparent; color:white;" cellspacing=0 rules='none'cellpadding=2 width=500px height=150px>
+<tr> <tr><td></td></tr>
+<tr> <td colspan=5 align='center' ><h3 class="h3 d-flex  mb-10" style="color:red">Please Login into your account to apply in any event.</h3></td></tr>
+<tr><td></td></tr>
+<tr><td></td></tr>
+<tr><td></td></tr>
+<tr><td></td></tr>
+<tr><td></td></tr>
+<tr><td></td></tr>
+<tr><td></td></tr>
+    <td align='left' width=50%>USERNAME</td>
+	<td colspan='3' align='left' width=50%><input type='text' size=25px placeholder='username' name='nm' value=''></td>
+  </tr>
+  <tr><td></td></tr>
+  <tr><td></td></tr>
+  <tr><td></td></tr>
+<tr><td></td></tr>
+<tr><td></td></tr>
+  <tr>
+    <td align='left'width=50%>PASSWORD</td>
+	<td colspan='3' align='left'width=50%><input type='password' size=25px placeholder='password' name='pass' value=''></td>
+  </tr>
+<tr> <tr><td></td></tr>
+<tr><td></td></tr>
+<tr><td></td></tr>
+<tr><td></td></tr>
+<tr><td></td></tr>
+<tr><td></td></tr>
+
+  
+   <td>
+     <input type='submit' value='LOGIN' name='login'>
+	</td></tr>
+</table>
+</form>
           </div>
         </div>
       </div>
@@ -90,78 +119,22 @@ session_start();
 
     
     
-<script>
-    $(document).ready(function() {
-    $('#details').DataTable();
-} );
-</script>
-    
-    <div class="site-section bg-dark" id="here">
-
-      <h1 align="center"> Details of the Event</h1>
-<table  id="details"   border='2' align='center' style="background:transparent;opacity:0.9;width:1000px;margin-top:-200px;height:800px;"  !important cellspacing=1  cellpadding=14>
-<thead>
-</thead>
- <tbody>
-      <?php
-
-              $no=$_REQUEST["a"];
-
-              $con=mysqli_connect("localhost","root","1234","sports");
-              $r=mysqli_query($con,"select * from eventlist where id='$no' ");
-
-
-              while($row=mysqli_fetch_array($r))
-              {
-
-echo "<tr><td><h3>ID: </td><td ></h3><h3>$row[0]</td></tr>"; ?></h3></br><?php
-echo "<tr><td><h3>NAME: </td><td ></h3><h3>$row[1]</td></tr>";?></h3></br><?php
-echo "<tr><td><h3>DATE: </td><td ></h3><h3>$row[2]</td></tr>";?></h3></br><?php
-echo "<tr><td><h3>TYPE: </td><td ></h3><h3>$row[3]</td></tr>";?></h3></br><?php
-echo "<tr><td><h3>DESCRIPTION: </td><td ></h3><h3>$row[4]</td></tr>";?></h3></br><?php
-echo "<tr><td><h3>ELIGIBILITY: </td><td ></h3><h3>$row[5]</td></tr>";?></h3></br><?php
-echo "<tr><td><h3>LOCATION: </td><td ></h3><h3>$row[6]</td></tr>";?></h3></br><?php
-echo "<tr><td><h3>FINAL DATE TO APPLY: </td><td></h3><h3>$row[7]</td></tr>";?></h3></br><?php
-
-}
-          ?>
-          </tbody></table>
-    </div> 
-
-    <div class="container" >
+    <div class="container">
       
 
-    <?php
-        if(isset($_SESSION["idd"])){ 
-          $con=mysqli_connect("localhost","root","1234","sports");
-              $r=mysqli_query($con,"select * from eventlist where id='$no' ");
-              $row=mysqli_fetch_array($r);
-              $use_this=$_SESSION["idd"];
-            
-        ?>
-
-        <p>
-              <a  href="apply.php?a=<?php echo $row[0];?>&b=<?php echo $use_this; ?>" class="btn btn-primary py-3 px-4 mr-3">Apply</a>
-              
-        </p>       
-        
-        <?php
-
-        } 
-        else { ?>
-        
-         <p>  <h3>You need to register yourself first for applying </h3>
-              <a href="register.php" class="btn btn-primary py-3 px-4 mr-3"> Register</a>
-              
-            </p>
-        <?php } ?>  
       <div class="row">
         <div class="col-lg-12">
+          
           
         </div>
       </div>
     </div>
-  <!-- .site-section -->
+  
+
+    
+    <div class="site-section bg-dark">
+      
+    </div> <!-- .site-section -->
 
     <div class="site-section">
       <div class="container">
